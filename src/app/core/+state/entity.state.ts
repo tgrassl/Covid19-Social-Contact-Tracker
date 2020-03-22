29ddde5f9ca6c1ec27a2320d12da5f5ab5956189
Timon Grassl <grassl.timon@gmail.com>
2020-03-22 23:52:34 +0100

    fix for release

--- src/app/core/+state/entity.state.ts
+++ src/app/core/+state/entity.state.ts
@@ -20,55 +20,7 @@ export interface EntityStateModel {
     indirectContacts: 0,
     medicalStatus: [],
     directContacts: [],
-    timeline: [
-      {
-        type: TimelineEventType.travel,
-        timestamp: moment('2020-03-02'),
-        from: '12:00',
-        to: '12:30',
-        indirectContacts: 12,
-        transportType: TransportType.train
-      },
-      {
-        type: TimelineEventType.checkIn,
-        timestamp: moment('2020-03-02T14:00:00')
-      },
-      {
-        type: TimelineEventType.contact,
-        timestamp: moment('2020-03-02'),
-        from: '16:00',
-        directContacts: 2,
-        contacts: [
-          {
-            phoneNumbers: null,
-            displayName: 'Alex',
-            thumbnail: 'https://pbs.twimg.com/profile_images/974736784906248192/gPZwCbdS.jpg'
-          },
-          {
-            phoneNumbers: null,
-            displayName: 'Anna',
-            thumbnail: 'https://images.unsplash.com/photo-1510227272981-87123e259b17?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max&s=3759e09a5b9fbe53088b23c615b6312e'
-          }
-        ]
-      },
-      {
-        type: TimelineEventType.checkOut,
-        timestamp: moment('2020-03-03T08:20:00')
-      },
-      {
-        type: TimelineEventType.activity,
-        timestamp: moment('2020-03-03T14:00:00'),
-        from: '15:40',
-        to: '16:60',
-        content: 'Rewe Einkauf',
-        indirectContacts: 8,
-      },
-      {
-        type: TimelineEventType.note,
-        timestamp: moment('2020-03-21T16:20:00'),
-        content: 'Noch keine Symptome, fühle mich gut'
-      },
-    ],
+    timeline: []
   }
 })
 export class EntityState {
