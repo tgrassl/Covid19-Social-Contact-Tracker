140a16256bd4c958fb19f070147fa5665cbc7284
Timon Grassl <grassl.timon@gmail.com>
2020-03-22 22:17:57 +0100

    add settings page

--- src/app/pages/settings/settings.page.ts
+++ src/app/pages/settings/settings.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { LocalNotifications } from '@ionic-native/local-notifications/ngx';
+import { LocalNotifications, ILocalNotification, ELocalNotificationTriggerUnit  } from '@ionic-native/local-notifications/ngx';
 
 @Component({
   selector: 'app-settings',
@@ -8,9 +8,48 @@ import { LocalNotifications } from '@ionic-native/local-notifications/ngx';
 })
 export class SettingsPage implements OnInit {
 
+  private notification: ILocalNotification = {
+    id: 1,
+    title: 'Heute schon getrackt?',
+    text: 'Vergiss nicht deine sozialen Kontakte oder Aktivitäten zu erfassen 🙌',
+    vibrate: true,
+    led: '#3399ff',
+    priority: 2,
+    trigger: {
+      every: ELocalNotificationTriggerUnit.DAY
+    }
+  };
+
+  public notificationsEnabled = false;
+
   constructor(private localNotifications: LocalNotifications) { }
 
   ngOnInit() {
+    this.checkIfNotificationsEnabled();
+  }
+
+  public checkIfNotificationsEnabled() {
+    this.localNotifications.getAll().then((notifications: ILocalNotification[]) => {
+      const found = notifications.find(notification => notification.id === this.notification.id);
+
+      if (found) {
+        this.notificationsEnabled = true;
+      }
+    });
+  }
+
+  public handleNotificationSetting(event): void {
+    this.notificationsEnabled = event.detail.checked;
+    this.scheduleNotification(this.notificationsEnabled);
+  }
+
+  public scheduleNotification(enabled: boolean): void {
+
+    if (enabled) {
+      this.localNotifications.schedule(this.notification);
+    } else {
+      this.localNotifications.cancel(this.notification.id);
+    }
   }
 
 }
